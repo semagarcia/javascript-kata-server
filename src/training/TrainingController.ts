c1ecae79a3f8e6ce395f2d80456f92c7412b6bda
Sema García <jose-manuel.garcia@gft.com>
2017-06-10 20:18:55 +0200

    Recreated main modules

---
+++ src/training/TrainingController.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response } from 'express';
+
+// Assign router to the express.Router() instance
+const router: Router = Router();
+
+/**
+ * Method:
+ * Route: 
+ */
+router.get('/', (req: Request, res: Response) => {
+    res.send('Hello, trainings!');
+});
+
+// Export the express.Router() instance
+export const TrainingController: Router = router;
