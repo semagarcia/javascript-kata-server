91254962d6c50588e6fdfe6838fde11faa649b14
Sema García <jose-manuel.garcia@gft.com>
2017-06-11 02:58:15 +0200

    Added users module

---
+++ src/users/UserController.ts
@@ -0,0 +1,71 @@
+import { Router, Request, Response } from 'express';
+import { UsersService } from './UserService';
+import { User, UserModel } from './../schemas/user';
+
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+const config = require('./../config.json');
+
+// Dependencies
+const usersSrv: UsersService = new UsersService();
+
+// Assign router to the express.Router() instance
+const userRouter: Router = Router();
+
+/**
+ * Method:
+ * Verb:
+ * Route: 
+ */
+userRouter.get('/', async(req: Request, res: Response) => {
+    await usersSrv.getAllUsers()
+        .then((users) => res.send(users))
+        .catch((err) => res.status(400).send('KO')); 
+});
+
+/**
+ * Method:
+ * Verb:
+ * Route: 
+ */
+userRouter.get('/user', async(req: Request, res: Response) => {
+    let token = req.headers.authorization.split(' ')[1];
+    let jwtInfo = jwt.verify(token, config.secretJwt);
+    await usersSrv.getUserInfoById(jwtInfo.sub)
+        .then((user) => res.send(user))
+        .catch((err) => res.status(400).send('KO'));
+});
+
+/**
+ * Method:
+ * Verb:
+ * Route: 
+ */
+userRouter.post('/', async(req: Request, res: Response) => {
+    await usersSrv.createUser({
+        name: req.body.name,
+        username: req.body.username, 
+        password: req.body.password,
+        email: req.body.email,
+        rol: req.body.rol
+    })
+        .then((user) => { res.status(200).send(user); })
+        .catch((err) => { res.status(400).send(err); });
+});
+
+/**
+ * Method:
+ * Verb:
+ * Route: 
+ */
+userRouter.put('/', async(req: Request, res: Response) => {});
+
+/**
+ * Method:
+ * Verb:
+ * Route: 
+ */
+userRouter.post('/:userId/delete', async(req: Request, res: Response) => {});
+
+// Export the express.Router() instance
+export const UserController: Router = userRouter;
---
+++ src/users/UserService.ts
@@ -0,0 +1,49 @@
+import { User, UserModel } from './../schemas/User';
+
+const bcrypt = require('bcryptjs');
+const config = require('./../config.json');
+const BCRYPT_SALT_ROUNDS = 12;
+
+export class UsersService {
+
+    async getAllUsers() {
+        return new Promise((resolve, reject) => {
+            UserModel.find({ enabled: true }, { password: 0 }, (err, users: Array<User>) => {
+                if(err) reject(err);
+                resolve(users);
+            });
+        });
+    }
+
+    async getUserInfoById(userId: string) {
+        return new Promise((resolve, reject) => {
+            UserModel.findById({ _id: userId }, { password: 0 }, (err, user: User) => {
+                if(err) reject(err);
+                resolve(user);
+            });
+        });
+    }
+
+    async createUser(userData) {
+        return new Promise((resolve, reject) => {
+            bcrypt.hash(userData.password, BCRYPT_SALT_ROUNDS)
+                .then((hashedPwd) => {
+                    return UserModel.create({
+                        name: userData.name,
+                        username: userData.username,
+                        password: hashedPwd,
+                        email: userData.email,
+                        rol: userData.rol
+                    });
+                })
+                .then(() => {
+                    resolve(true);
+                })
+                .catch((err) => {
+                    console.log('Error: ', err);
+                    reject(err);
+                });
+        });
+    }
+
+}
