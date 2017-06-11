f01c3bd013e1afa530256c22dee8ada30f8ca3c9
Sema García <jose-manuel.garcia@gft.com>
2017-06-11 15:14:45 +0200

    Added challenge implementation

--- src/challenges/ChallengeController.ts
+++ src/challenges/ChallengeController.ts
@@ -1,15 +1,74 @@
 import { Router, Request, Response } from 'express';
+import { ChallengeService } from './ChallengeService';
+
+// Dependencies
+const challengeSrv: ChallengeService = new ChallengeService();
 
 // Assign router to the express.Router() instance
-const router: Router = Router();
+const challengeRouter: Router = Router();
 
 /**
  * Method:
- * Route: 
+ * Verb:
+ * Route: /api/challenges
  */
-router.get('/', (req: Request, res: Response) => {
-    res.send('Hello, challenges!');
+challengeRouter.get('/', async(req: Request, res: Response) => {
+    // { challenges:Array.from(challenges) }
+    await challengeSrv.getChallenges()
+        .then((challenges) => res.status(200).send(challenges))
+        .catch((err) => res.status(400).send('KO'));
+});
+
+/**
+ * Method:
+ * Verb:
+ * Route: /api/challenges/challenge/:challengeId
+ */
+challengeRouter.get('/challenge/:challengeId', async(req: Request, res: Response) => {
+    // { challenge: challenge }
+    await challengeSrv.getChallenge(req.params.challengeId)
+        .then((challenge) => res.status(200).send(challenge))
+        .catch((err) => res.status(400).send('KO'));
+});
+
+/**
+ * Method:
+ * Verb:
+ * Route: /api/challenges/create
+ */
+challengeRouter.post('/create', async(req: Request, res: Response) => {
+    if(req.body.playerId, req.body.direction && req.body.duration >= 0 && req.body.mode) {
+            await challengeSrv.createNewChallenge(
+                req.body.playerId, req.body.direction, req.body.duration, req.body.mode, null)
+                .then((uuid) => { res.send({ uuid: uuid }) })
+                .catch((err) => { res.status(500).send({ error: 'C1OP3' }) });
+        } else {
+            res.status(500).send('Minimum fields not filled');
+        }
+});
+
+/**
+ * Method:
+ * Verb:
+ * Route: /api/challenges/check-challenge-id/:challengeId
+ */
+challengeRouter.get('/', async(req: Request, res: Response) => {
+    // { exists:existsChallengeId }
+    await challengeSrv.checkChallengeId(req.params.challengeId)
+        .then((existsChallengeId) => res.status(200).send(existsChallengeId))
+        .catch((err) => res.status(400).send('KO'));
+});
+
+/**
+ * Method:
+ * Verb:
+ * Route: /api/challenges/join
+ */
+challengeRouter.post('/', async(req: Request, res: Response) => {
+    await challengeSrv.joinPlayerIntoChallenge(req.body.challengeId, req.body.username, req.body.playerId)
+        .then((challenge) => res.status(200).send(challenge))
+        .catch((err) => res.status(400).send('KO'));
 });
 
 // Export the express.Router() instance
-export const ChallengesController: Router = router;
+export const ChallengesController: Router = challengeRouter;
---
+++ src/challenges/ChallengeService.ts
@@ -0,0 +1,103 @@
+import { IndividualService } from './../individual/IndividualService';
+import { Challenge, ChallengeModel } from './../schemas/Challenge';
+
+const uuidV4 = require('node-uuid');
+const moment = require('moment');
+
+export class ChallengeService {
+
+    getChallenges(): Promise<Map<string, Challenge>> {
+        return new Promise((resolve, reject) => {
+            ChallengeModel.find({ 
+                'created_at': { 
+                    '$gte': moment().subtract(24, 'hours').toDate() 
+                } 
+            }).exec((err, challenges) => {
+                if(err) reject(err);
+                resolve(challenges)
+            });
+        });
+    }
+
+    getChallenge(challengeId: string): Promise<Challenge> {
+        return new Promise((resolve, reject) => {
+            ChallengeModel.findOne({ challengeId: challengeId }, {}, (err, challenge) => {
+                if(err) reject(err);
+                resolve((challenge) ? challenge : {});
+            });
+        });
+    }
+
+    createNewChallenge(playerSocketId: string, direction: string, duration: number, mode: string, event: string): Promise<String> {
+        return new Promise((resolve, reject) => {
+            let challengeUUID = uuidV4().slice(0, 8);  // Save only the first 8 characters
+            individualKataSrv.getRandomIndividualKata()
+                .then((randomKata) => {
+                    ChallengeModel.create({
+                        challengeId: challengeUUID,
+                        direction: direction,
+                        duration: duration,
+                        mode: mode,
+                        event: event,
+                        creator: playerSocketId,
+                        challengeKata: randomKata._id
+                    }, (err, challenge) => {
+                        if(err) { reject(err); }
+                        resolve(challengeUUID);
+                    });
+                })
+                .catch((err) => { 
+                    console.log(`Error assigning kata for challenge (${err})`);
+                    reject(`Error assigning kata for challenge (${err})`);
+                });
+        });
+    }
+
+    checkChallengeId(challengeId:string): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            ChallengeModel.findOne({ challengeId: challengeId }, {}, (err, challenge) => {
+                if(err) reject(err);
+                resolve(true);
+            });
+        });
+    }
+
+    joinPlayerIntoChallenge(challengeId: string, username: string, playerId: string) {
+        return new Promise((resolve, reject) => {
+            ChallengeModel.findOne({ challengeId: challengeId }, (err, challenge) => {
+                if(err) reject(err);
+                if(challenge && challenge.status !== 'WAITING') {
+                    // Playing or not valid challenge (expired or ended); please, create a new one
+                    console.log('playing or not valid challenge');
+                    reject(false);
+                } else if(challenge && challenge.status === 'WAITING' && !challenge.playerA) {  
+                    challenge.status = 'WAITING';
+                    challenge.playerA = playerId;
+                    challenge.usernamePlayerA = username;
+                    console.log('PlayerA joined');
+                    this.saveChallengeUpdate(challenge, resolve, reject);
+                } else if(challenge && challenge.status === 'WAITING' && challenge.playerA && !challenge.playerB) {
+                    // Allow more than one player??????????????
+                    challenge.status = 'PLAYING';
+                    challenge.playerB = playerId;
+                    challenge.usernamePlayerB = username;
+                    console.log('PlayerB joined');
+                    //this.streamingGtw.startSyncChallenge(challenge.challengeId, 'playerB has joined into challenge');
+                    this.saveChallengeUpdate(challenge, resolve, reject);
+                } else 
+                    reject(false);
+            })
+        });
+    }
+
+    private saveChallengeUpdate(c, rs, rj) {
+        c.save((err, c, nRows) => {
+            if(err) rj(err);
+            if(c && nRows === 1) 
+                rs(c);
+            else 
+                rs(false);
+        });
+    }
+
+}
