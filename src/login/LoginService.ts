095f7fd07b104cc00d70790ac176c522743589ed
Pablo Molina <p2kmgcl@gmail.com>
2017-06-24 10:55:01 +0200

    Fixed unexisting user login
    Now it returns a 401 and stops checking password if the user is
    not in database.

--- src/login/LoginService.ts
+++ src/login/LoginService.ts
@@ -10,16 +10,21 @@ export class LoginService {
     async standardLogin(username: string, password: string) {
         return new Promise((resolve, reject) => {
             UserModel.findOne({ username: username, enabled: true }, (err, user: User) => {
-                if(err || !user) reject(`Error at login: ${err}`);
+                if(err) reject(`Error at login: ${err}`);
+                if(!user) {
+                    console.log(`Unexisting user: ${username}`);
+                    return reject('Error at login');
+                }
+
                 console.log('Checking user: ', user);
                 bcrypt.compare(password, user.password)
                     .then((samePasswords) => {
                         if(samePasswords) {
-                            resolve({ 
+                            resolve({
                                 name: user.name,
                                 username: user.username,
                                 rol: user.rol,
-                                token: jwt.sign({ 
+                                token: jwt.sign({
                                     sub: user._id,  // Identifies token
                                     iat: moment().unix(),  // Token creation data,
                                     exp: moment().add(60, 'minutes').unix(),  // Expires in 60 minutes
