c4006f811ff1249b96157695b644e279eafd4cd2
Sema García <jose-manuel.garcia@gft.com>
2017-06-25 22:48:07 +0200

    * Moved the VM sandbox initialization to avoid repeat this process in each test execution * Updated log implementation to take in account when console.log is called with N arguments

--- src/katas/KatasService.ts
+++ src/katas/KatasService.ts
@@ -1,6 +1,26 @@
 import { Kata, KataModel } from './../schemas/Kata';
 import { KataStats, KataStatsModel } from './../schemas/Statistics';
-const { VM } = require('vm2');
+
+let log;
+const { NodeVM } = require('vm2');
+const vm = new NodeVM({
+    timeout: 4000,
+    sandbox: {
+        deepEqual: require('assert').deepEqual,
+        console: {
+            log: (...args) => {
+                if(args && args.length === 1) {
+                    // The console.log() call is composed by only one argument (no need separator)
+                    log += JSON.stringify(args[0]).slice(1, -1);
+                } else if(args && args.length > 1) {
+                    // Pass over each argument
+                    log += '#';  // Specific separator for each console.log() argument
+                    args.forEach(arg => log += JSON.stringify(arg).replace(/"/g, '').concat(' '));
+                }
+            }
+        }
+    }
+});
 
 export class KatasService {
 
@@ -60,26 +80,7 @@ export class KatasService {
      */
     async executeTest(kataFunction: string, kataName: string) {
         return new Promise(async(resolve, reject) => {       
-            let log;
-            const vm = new VM({
-                timeout: 4000,
-                sandbox: {
-                    deepEqual: require('assert').deepEqual,
-                    console: {
-                        log: (...args) => {
-                            if(args && args.length === 1) {
-                                // The console.log() call is composed by only one argument (no need separator)
-                                log += JSON.stringify(args[0]).slice(1, -1);
-                            } else if(args && args.length > 1) {
-                                // Pass over each argument
-                                args.forEach(arg => log += JSON.stringify(arg).slice(1, -1));
-                                log += '#';  // End of each-individual console.log() call (specific separator)
-                            }
-                        }
-                    }
-                }
-            });
-
+            log = '';
             KataModel.findOne({ name: kataName, enabled: true }, 'tests', (err, kata: Kata) => {
                 if(err) reject(err);
 
@@ -93,6 +94,7 @@ export class KatasService {
                     } catch(err) {
                         test['result'] = false;
                         test['message'] = err.toString();
+                        console.log('ERR: ', err);
                     }
                     test['log'] = log;
                 }
