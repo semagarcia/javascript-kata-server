2bed09afd11ebfa383113bc7a3a0515da0aded9a
semagarcia <jmanuelgarciagarcia@gmail.com>
2017-06-29 23:52:36 +0200

    Added JWT entries

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port:   process.env.PORT || 3000,
     db:     process.env.MONGODB_URI || 'mongodb://localhost:27017/kata-player',
-    secretToken: process.env.SECRET_TOKEN || 'claveeeee',
-    secretJWT: process.env.SECRET_JWT || 'keeyyyyyy'
+    secretJWT: process.env.SECRET_JWT || 'keeyyyyyy',
+    jwtExpiration: process.env.JWT_EXP || 10080
 }
