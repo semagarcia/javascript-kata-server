d25fd9abdf8dfaaf62c13f243aaa34e09535b03e
Sema García <jose-manuel.garcia@gft.com>
2017-07-01 18:54:18 +0200

    Reduced time session

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     port:   process.env.PORT || 3000,
     db:     process.env.MONGODB_URI || 'mongodb://localhost:27017/kata-player',
     secretJWT: process.env.SECRET_JWT || 'keeyyyyyy',
-    jwtExpiration: process.env.JWT_EXP || 10080
+    jwtExpiration: process.env.JWT_EXP || 300
 }
