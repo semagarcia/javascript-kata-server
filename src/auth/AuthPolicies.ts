907b4b49d535d91553d2039aa28bffc7357caf53
Sema García <jose-manuel.garcia@gft.com>
2017-07-03 02:56:02 +0200

    * Added ROLES reference * Added header for requiresRole * Added implementation for requiresRole

--- src/auth/AuthPolicies.ts
+++ src/auth/AuthPolicies.ts
@@ -1,10 +1,16 @@
 import { AuthUtil } from './AuthUtil';
+import { ROLES } from './../schemas/User';
 
 export class AuthPolicies {
 
     /**
+     * Reference to roles defined
+     */
+    static ROLES = ROLES;
+
+    /**
      * This method will check if the user is authenticated, if the security header exists and also
-     * verifies if is valid
+     * verifies if it is valid
      * @param req Request's user object
      * @param res Response object
      * @param next Function to execute the next middleware
@@ -21,17 +27,30 @@ export class AuthPolicies {
             });
     };
 
+    /**
+     * The "req.user" user session object is available because previous to this, we have to set
+     * the "passport.authorize('jwt')" as needed. This middleware is who will put the user info
+     * into the session (deserialized)
+     * @param role Role to check
+     */
     static requiresRole(role: string) {
         return (req, res, next) => {
-            console.log('>> ROLES => ', role);
-            console.log('>> session => ', req.user);
-            if(req.user && req.user.role === role)
+            if(!req.user) {
+                // There is no session, error
+                res.status(401).json({
+                    error: 'ER-L-104',
+                    message: 'You have to be logged'
+                });
+            } else if(req.user && req.user.role === role) {
+                // Validation and check OK
                 next();
-            else
+            } else {
+                // Validation and check error
                 res.status(401).json({
                     error: 'ER-L-104',
                     message: 'You are not authorized'
                 });
+            }
         };
     }
 
