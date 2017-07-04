c213a2640af853c38f19532d9e6fea70550d3ed1
Sema García <jose-manuel.garcia@gft.com>
2017-07-04 04:33:44 +0200

    Update session expiration

--- src/config.js
+++ src/config.js
@@ -2,5 +2,11 @@ module.exports = {
     port:   process.env.PORT || 3000,
     db:     process.env.MONGODB_URI || 'mongodb://localhost:27017/kata-player',
     secretJWT: process.env.SECRET_JWT || 'keeyyyyyy',
-    jwtExpiration: process.env.JWT_EXP || 300
+    jwtExpiration: process.env.JWT_EXP || 1800,
+    github: {
+        clientID: process.env.GITHUB_CLIENTID || 'b1e8259c8492bb8d6483',
+        clientSecret: process.env.GITHUB_CLIENT_SECRET || 'ccde7e00075e1bdf2b127a1e8013c3830f51df7a',
+        callbackURL: process.env.GITHUB_CALLBACK_URL || 'http://127.0.0.1:3000/auth/github/callback'
+    },
+    twitter: {}
 }
