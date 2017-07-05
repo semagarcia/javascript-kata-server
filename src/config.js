181ba164fcee724aa46cff4d6b2e425b73e4ec10
Sema García <jose-manuel.garcia@gft.com>
2017-07-05 17:47:59 +0200

    Updated config

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ module.exports = {
     port:   process.env.PORT || 3000,
     db:     process.env.MONGODB_URI || 'mongodb://localhost:27017/kata-player',
     secretJWT: process.env.SECRET_JWT || 'keeyyyyyy',
-    jwtExpiration: process.env.JWT_EXP || 1800,
+    jwtExpiration: process.env.JWT_EXP || '2h',
     github: {
-        clientID: process.env.GITHUB_CLIENTID || 'b1e8259c8492bb8d6483',
-        clientSecret: process.env.GITHUB_CLIENT_SECRET || 'ccde7e00075e1bdf2b127a1e8013c3830f51df7a',
+        clientID: process.env.GITHUB_CLIENTID || '56d0a13332e7e192ca98',
+        clientSecret: process.env.GITHUB_CLIENT_SECRET || 'db9b314ca7cf2aa7eba5fd2f22afe5ff46f4c279',
         callbackURL: process.env.GITHUB_CALLBACK_URL || 'http://127.0.0.1:3000/auth/github/callback'
     },
     twitter: {}
